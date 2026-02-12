8d9ee799b2fe9ba85f46a9a625fd80654af2cfa0
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-02-12 09:27:44 +0530

    set query

--- model_migration/blog/models.py
+++ model_migration/blog/models.py
@@ -5,4 +5,8 @@ from django.db import models
 class Student(models.Model):
     name=models.CharField(20)
     age=models.IntegerField()
-    city=models.CharField(20)   
+    city=models.CharField(20)  
+    # used to convert the query set into readable format
+
+def __str__(self):
+    return self.name
