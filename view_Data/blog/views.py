9be85c2c1415102cdd4d36962db112a05d7ebc09
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-02-26 08:32:57 +0530

    displaying database records on view

--- view_Data/blog/views.py
+++ view_Data/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Student
 
+
+
+obj=Student.objects.all()
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{"data":obj})
