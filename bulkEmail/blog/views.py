caf8b1dfecb2fd003182c06a72c0c397a4ddba4e
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-05-25 13:42:46 +0530

    bulk email

--- bulkEmail/blog/views.py
+++ bulkEmail/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mass_mail
+from django.http import HttpResponse
 # Create your views here.
+
+def bulk(request):
+    message1= ("hello user " , "Welcome to our website","kaushalraut.code@gmail.com",["kaushalraut755@gmail.com"])
+
+    send_mass_mail((message1,))
+    return HttpResponse("Mail sent successfully")
--- bulkEmail/bulkEmail/settings.py
+++ bulkEmail/bulkEmail/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bulkEmail.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='Kaushalraut.code@gmail.com'
+EMAIL_HOST_PASSWORD='bvpz tcxh ujec hisd'
+DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
